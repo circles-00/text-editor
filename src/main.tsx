eb6e797345327871a4324c51156e25266161b763
Nikola <nkrezeski@banzae.dev>
2023-02-15 03:44:02 +0100

    feat: Setup tailwind - Remove saas

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 // import './samples/node-api'
-import "./index.scss";
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
-postMessage({ payload: "removeLoading" }, "*");
+postMessage({ payload: 'removeLoading' }, '*')
